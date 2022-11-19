9880d34155ea8947c8d0b995d3827309b716a7a7
yuval1121 <yuvalvarshevsky@gmail.com>
2022-11-19 12:02:07 +0200

    added config

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, Button } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Hello</Text>
+        <Button onPress={() => console.log('clicked')}>
+          <Text>Press me</Text>
+          test
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: 'white',
+    flex: 1,
     justifyContent: 'center',
   },
 });
