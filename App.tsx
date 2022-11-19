c344487753d77a2b3034867b4c81149d11c9f8ff
yuval1121 <yuvalvarshevsky@gmail.com>
2022-11-19 12:09:30 +0200

    added react native eslint

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider as PaperProvider, Button } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
         <Text>Hello</Text>
-        <Button onPress={() => console.log('clicked')}>
-          <Text>Press me</Text>
-          test
-        </Button>
       </View>
     </PaperProvider>
   );
