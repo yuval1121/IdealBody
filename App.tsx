78b115e21c8ed9aae4b3b4b7406d51a2f1accb38
yuval1121 <yuvalvarshevsky@gmail.com>
2022-11-22 21:51:25 +0200

    another test

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
-  const t = 7;
   return (
     <PaperProvider>
       <View style={styles.container}>
