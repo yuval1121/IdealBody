92acfb18a7338ef853b071a05285ca8a1b931418
yuval1121 <yuvalvarshevsky@gmail.com>
2022-12-10 15:36:32 +0200

    added .env support

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          safe: true,
+          allowUndefined: false,
+        },
+      ],
+    ],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],
