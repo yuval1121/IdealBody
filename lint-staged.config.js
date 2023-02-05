0dd7a6ce8cae69d5df8ecdc72325e15a6fce37d2
yuval1121 <yuvalvarshevsky@gmail.com>
2023-02-05 12:15:26 +0200

    updated lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,8 +2,8 @@ module.exports = {
   '*.{js,jsx,json}': ['prettier --write'],
   '*.{ts,tsx}': [
     () => 'tsc --noEmit --skipLibCheck --jsx react-native',
-    'organize-imports-cli',
     'eslint --fix',
+    'organize-imports-cli',
     'prettier --write',
   ],
 };
