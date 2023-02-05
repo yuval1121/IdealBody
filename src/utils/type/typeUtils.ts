138848d16385cee3bf8265aa90b1c1beb3a8bead
yuval1121 <yuvalvarshevsky@gmail.com>
2023-02-05 15:27:55 +0200

    refactored type utils

--- src/utils/type/typeUtils.ts
+++ src/utils/type/typeUtils.ts
@@ -1,6 +1,5 @@
 export type MakePropertyOptional<T, K extends keyof T> = Pick<Partial<T>, K> &
   Omit<T, K>;
 
-export type MakePropertyRequired<T, K extends keyof T> = T & {
-  [P in K]-?: T[P];
-};
+export type MakePropertyRequired<T, K extends keyof T> = Pick<Required<T>, K> &
+  Omit<T, K>;
