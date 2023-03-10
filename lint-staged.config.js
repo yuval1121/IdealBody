a518531b907403105ca5851139ac6f77ae13e127
yuval1121 <yuvalvarshevsky@gmail.com>
2023-03-10 18:05:06 +0200

    moved tsc to pre-push hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   '*.{js,jsx,json}': ['prettier --write'],
-  '*.{ts,tsx}': [
-    () => 'tsc --noEmit --skipLibCheck --jsx react-native',
-    'eslint --fix',
-    'prettier --write',
-  ],
+  '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
 };
