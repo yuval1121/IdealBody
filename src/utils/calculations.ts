393707781a5aaa5aa1c2b01545ee5beadee1cfc9
yuval1121 <yuvalvarshevsky@gmail.com>
2023-03-10 15:13:42 +0200

    added util

--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -1,3 +1,7 @@
 export const calculateBMI = (height: number, weight: number) => {
-  return parseFloat((weight / height ** 2).toFixed(3));
+  try {
+    return parseFloat((weight / height ** 2).toFixed(3));
+  } catch (e) {
+    return 0;
+  }
 };
