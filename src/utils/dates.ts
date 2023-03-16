669b37803cdffd7dd77826d358db95c744855dc6
yuval1121 <yuvalvarshevsky@gmail.com>
2023-03-17 00:57:25 +0200

    removed reverse

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -3,12 +3,12 @@ import dayjs from 'dayjs';
 export const getLast6Days = () => {
   const days = [];
 
-  for (let i = 0; i < 6; i++) {
+  for (let i = 5; i >= 0; i--) {
     const day = dayjs().subtract(i, 'd');
     days.push(day);
   }
 
-  return days.reverse();
+  return days;
 };
 
 export const getLast6DaysLabels = () => {
