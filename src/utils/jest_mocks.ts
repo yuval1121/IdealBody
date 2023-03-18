8309ff5dd12ed81d2f2d92410e2d16f9a33dfdaa
yuval1121 <yuvalvarshevsky@gmail.com>
2023-03-18 12:28:41 +0200

    improved tests

--- src/utils/jest_mocks.ts
+++ src/utils/jest_mocks.ts
@@ -1,15 +1,15 @@
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+// jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
-jest.mock('@react-navigation/native', () => {
-  return {
-    useNavigation: () => ({
-      navigate: jest.fn(),
-      dispatch: jest.fn(),
-    }),
-    useRoute: () => ({
-      params: {
-        id: '123',
-      },
-    }),
-  };
-});
+// jest.mock('@react-navigation/native', () => {
+//   return {
+//     useNavigation: () => ({
+//       navigate: jest.fn(),
+//       dispatch: jest.fn(),
+//     }),
+//     useRoute: () => ({
+//       params: {
+//         id: '123',
+//       },
+//     }),
+//   };
+// });
