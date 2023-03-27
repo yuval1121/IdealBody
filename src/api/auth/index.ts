25971fff295f79c987670d61c60eac4a403ac035
yuval1121 <yuvalvarshevsky@gmail.com>
2023-03-27 21:37:47 +0300

    added reset password

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,6 +1,7 @@
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  sendPasswordResetEmail,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { UserAuth } from './types';
@@ -15,4 +16,9 @@ export const signUserIn = async ({ email, password }: UserAuth) => {
   const auth = getAuth();
   const { user } = await signInWithEmailAndPassword(auth, email, password);
   return user;
+};
+
+export const resetPassword = async (email: string) => {
+  const auth = getAuth();
+  await sendPasswordResetEmail(auth, email);
 };
