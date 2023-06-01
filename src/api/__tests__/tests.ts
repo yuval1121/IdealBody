c1104ae361da5b2c026564a57e0982acbff8c53d
yuval1121 <yuvalvarshevsky@gmail.com>
2023-06-01 18:21:49 +0300

    added api tests

--- src/api/__tests__/tests.ts
+++ src/api/__tests__/tests.ts
@@ -1,15 +1,68 @@
-import { getAuth } from 'firebase/auth';
-import { createUserWithEmailAndPassword } from 'firebase/auth/react-native';
+import { API_TOKEN, PROJECT_ID } from '@env';
+import crypto from 'crypto';
 import { enterPrompt } from '../chatgpt';
 
 describe('API tests', () => {
-  // it('ChatGPT api test', async () => {
-  //   const res = await enterPrompt('Hello');
-  //   expect(res).toBeTruthy();
-  // });
+  it('ChatGPT test', async () => {
+    const res = await enterPrompt('Hello');
+    expect(res).toBeTruthy();
+  });
 
-  it('Auth tests', async () => {
-    const auth = getAuth();
-    const { user } = await createUserWithEmailAndPassword(auth, 'foo', 'foo');
+  it('Sign in test', async () => {
+    const res = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_TOKEN}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({ email: 'c@c.com', password: '123456' }),
+      }
+    );
+    expect(res.ok).toBe(true);
+    const data = await res.json();
+    expect(data).toHaveProperty('idToken');
+  });
+
+  it('Sign up test', async () => {
+    const email = `${crypto.randomUUID()}@${crypto.randomUUID()}.com`;
+    const password = crypto.randomUUID();
+    const res = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_TOKEN}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+      }
+    );
+    expect(res.ok).toBe(true);
+    const data = await res.json();
+    expect(data).toHaveProperty('idToken');
+  });
+
+  it('Firestore get data', async () => {
+    const authRes = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_TOKEN}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: 'c@c.com',
+          password: '123456',
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const authData = await authRes.json();
+    const res = await fetch(
+      `https://firestore.googleapis.com/v1/projects/${PROJECT_ID}/databases/(default)/documents/users/${authData.localId}
+      `,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${authData.idToken}`,
+        },
+      }
+    );
+    const { fields } = await res.json();
+    expect(fields).toHaveProperty('caloriesOut');
+    expect(fields).toHaveProperty('weight');
+    expect(fields).toHaveProperty('height');
   });
 });
